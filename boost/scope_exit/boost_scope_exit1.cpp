cfc45204f59cad26fc02cc7314ebc4934ec50996
Nicola Bernini <NicolaBernini@users.noreply.github.com>
2016-01-30 13:03:57 +0100

    Create boost_scope_exit1.cpp

---
+++ boost/scope_exit/boost_scope_exit1.cpp
@@ -0,0 +1,32 @@
+#include <boost/scope_exit.hpp>
+#include <iostream>
+
+/**
+  * @brief Testing BOOST_SCOPE_EXIT macro to trap the scope end 
+  * @warning As the macro should run before return, it should return NULL instead it returns a pointer to a heap memory region which has been correctly deallocated 
+  */
+std::string *foo()
+{
+  std::string *i = new std::string("Test");
+  BOOST_SCOPE_EXIT(&i)
+  {
+    delete i;
+    i = NULL;
+    std::cout << "[BOOST_SCOPE_EXIT] Delete Successful and pointer set to NULL " << std::endl; 
+  } BOOST_SCOPE_EXIT_END
+  std::cout << "*i = " << *i << '\n';
+  std::cout << "i = " << i << '\n';
+  return i;
+}
+
+int main()
+{
+  std::string *j = foo();
+  std::cout << "j = " << j << '\n';
+  //** If the pointer is not NULL I can assume it's valid while it is not as the delete has been performed 
+  //** Uncomment this and get SEGFAULT produced by invalid pointer dereferencing 
+  /*
+  if(j != NULL)
+      std::cout << "*j = " << *j << '\n';
+   */
+}
