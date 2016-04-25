58a6494dded3450127851c2e7546e1d68f2761a3
Nicola Bernini <NicolaBernini@users.noreply.github.com>
2016-04-25 16:11:36 +0200

    Update wt1.cpp

--- boost/thread/worker_thread/wt1.cpp
+++ boost/thread/worker_thread/wt1.cpp
@@ -3,21 +3,34 @@
 #include <queue>
 #include <boost/date_time/posix_time/posix_time.hpp>
 
+/**
+  * @brief The Class that contains the Producer Consumer double thread solution 
+  */
 class Test
 {
 public:
+    /**
+      * @brief The Default Constructor creates the Worker or Consumer Thread which waits for Data to be processed 
+      */
     Test()
     {
         // Worker Thread containing the Blocking Function 
         thread = boost::thread(boost::bind(&Test::thread_func, this));
     }
-
+    
+    /**
+      * @brief The Destructor is triggered by the object destroying event and it manages the Worker Thread closing 
+      * @note The Worker Thread is interrupted and a sync join is performed 
+      */
     ~Test()
     {
-        //thread.interrupt();
+        thread.interrupt();
         thread.join();
     }
-
+    
+    /**
+      * @brief The Producer Thread (main thread) gets the mutex when pushing data into the shared queue appunto 
+      */
     void run()
     {
         for (size_t i = 0; i < 10; ++i) {
@@ -29,20 +42,24 @@ public:
 
 private:
     /**
-      * @brief The Worker Thread which processes just the last element 
+      * @brief The Worker Thread Function which processes just the last element 
+      * @note The choice to process just the last element is by design and can be easily changed 
       */
     void thread_func()
     {
+        //** Gets out of the while when interruption is requested 
         while (!boost::this_thread::interruption_requested())
         {
+            //** Internal scope to perform scoped lock 
             {
                 boost::unique_lock<boost::mutex> lock(mutex);
                 std::cout << "Processing Element" << std::endl; 
                 //** Waiting for an element 
                 while (queue.empty())
+                    //** Mutex release while sleeping and mutex take when awake exploiting the condition variable facility 
                     condition_var.wait(lock);
                 std::cout << "Got element " << queue.front() << std::endl; 
-                //** Dropping the othe commands 
+                //** Dropping the other commands 
                 while(!queue.empty())
                     queue.pop();
             }
@@ -50,15 +67,32 @@ private:
     }
 
 private:
+    /**
+      * @brief The Worker Thread 
+      */
     boost::thread thread;
+    
+    /**
+      * @brief The Sync Mutex to Access the Shared Queue 
+      */
     boost::mutex mutex;
+    
+    /**
+      * @brief The Condition Variable to access the Data efficiently 
+      */
     boost::condition_variable condition_var;
+    
+    /**
+      * @brief The Shared Data Queue appunto 
+      */
     std::queue<int> queue;
 };
 
 int main()
 {
+    //** Creates the object and Worker thread is ready 
     Test test;
+    //** Runs the Producer which after pushing data awakes the worker to perform processing 
     test.run();
     
     //boost::this_thread::sleep(boost::posix_time::milliseconds(100));
