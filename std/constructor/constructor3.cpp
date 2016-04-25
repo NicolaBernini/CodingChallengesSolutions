f5d799ea8043439e757745d4bc9a47c07e1b3b91
Nicola Bernini <NicolaBernini@users.noreply.github.com>
2016-04-25 11:27:29 +0200

    Update constructor3.cpp

--- std/constructor/constructor3.cpp
+++ std/constructor/constructor3.cpp
@@ -1,3 +1,7 @@
+/**
+  * @brief Another example regarding Constructor Ambiguity in case of Default Arguments 
+  * @note To see it running visit http://ideone.com/GjFzxS 
+  */
 #include <iostream>
 
 #define STR_DEF_ARG 
@@ -6,17 +10,25 @@
 class A
 {
     public: 
-    
+    /**
+      * @brief The Constructor with no Arguments 
+      */
     A() : m_str("")
     {
         std::cout << "Default Constructor with no arguments internally invoking std::string(const char*) constructor" << std::endl; 
     }
     
+    /**
+      * @brief The Constructor taking a String and internally performing a copy 
+      */
     A(const std::string& in_str STR_DEF_ARG) : m_str(in_str) 
     {
         std::cout << "Constructor const std::string& internally invoking std::string copy constructor " << std::endl; 
     }
     
+    /**
+      * @brief The Constructor taking a C String (hence an array of char) and internally building a String with it 
+      */
     A(const char* in_str STR_DEF_ARG) : m_str(in_str)
     {
         std::cout << "Constructor const char* internally invoking std::string(const char*) constructor" << std::endl; 
