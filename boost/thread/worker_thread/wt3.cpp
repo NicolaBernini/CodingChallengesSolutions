295b53d8cf499232040879c8ffe8114f5f0dacaf
Nicola Bernini <NicolaBernini@users.noreply.github.com>
2016-04-25 16:19:08 +0200

    Update wt3.cpp

--- boost/thread/worker_thread/wt3.cpp
+++ boost/thread/worker_thread/wt3.cpp
@@ -1,10 +1,22 @@
+/**
+  * @file wt3.cpp 
+  * @brief The main thread creates up to N = 1 parallel threads and detaches them (so noi sync join is involved) 
+  * @note To see it running check at http://coliru.stacked-crooked.com/a/ee517476a341ed4e 
+  */
+
 #include <iostream>
 #include <boost/thread/thread.hpp>
 #include <boost/function.hpp>
 #include <boost/bind.hpp>
 
+/**
+  * @brief The Global Signaling Variable which identifies how many Worker Threads are currently running 
+  */
 int cont=0; 
 
+/**
+  * @brief The Worker Thread Function 
+  */
 void f_thread1(int i)
 {
     std::cout << "[Thread " << i << "] Started cont = " << cont << std::endl; 
@@ -17,15 +29,16 @@ int main()
 {   
     for(int i=0; i<32; ++i)
     {
+        //** Runs a new Worker Thread only if the number of currently running Worker Threads is zero 
         if(cont==0)
         {
             cont++; 
+            //** Thread is created and detached as there is no need to join it 
             auto t = new boost::thread(boost::bind(f_thread1, i)); 
             t->detach();
         }
         boost::this_thread::sleep(boost::posix_time::milliseconds(3));
     }
-    //std::cout << "Out of the for " << std::endl; 
     
 }
 
