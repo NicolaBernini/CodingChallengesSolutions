9898a94c7e3b873d5e15eb1a4f5faa8a64977e06
Nicola Bernini <NicolaBernini@users.noreply.github.com>
2017-11-05 11:20:53 +0100

    Update sum1.cpp

--- leetcode/sum1.cpp
+++ leetcode/sum1.cpp
@@ -8,15 +8,29 @@
 using namespace std;
 
 
+/**
+  * @brief Solution 
+  * Predicate List 
+  * p_pair_no_order = Computes all the possible pairs with no order distinction and no same element repetition, out of a list of elements 
+  * p_criteria_sum_equal = It takes the final sum result as Param and checks it equals the result of sum applied to an element list (in this case a pair)
+  * p_get_first = Takes the first element of a list 
+  */
+
 class Solution {
 public:
     static vector<int> twoSum(vector<int>& nums, int target) {
+        // pairs = map(p_pair_no_order, vector)
+        // The full set of possibilities is given by a NxN Square Matrix and we are interested only in the upper/lower half matrix excluded the diagonal 
         for(int i=1; i<nums.size(); ++i)
         {
             for(int j=0; j<i; j++)
             {
-            	//std::cout << "Checking i = " << i << ", j = " << j << " --> " << nums[i] << ", " << nums[j] << std::endl; 
-                if((nums[i] + nums[j]) == target) return std::vector<int>({i,j}); 
+                //// DEPRECATED 
+            	////std::cout << "Checking i = " << i << ", j = " << j << " --> " << nums[i] << ", " << nums[j] << std::endl; 
+                
+                // The (i,j) is a valid pair 
+                // result = map(p_get_first, filter( bind(p_criteria_sum_equal, target_val), pairs ) )
+                if((nums[i] + nums[j]) == target) return std::vector<int>({j,i}); 
             }
         }
         
