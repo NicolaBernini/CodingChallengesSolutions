28eb5f578a588b2a52ff35806b3b0d7a96e8a541
Nicola Bernini <NicolaBernini@users.noreply.github.com>
2021-12-08 13:21:40 +0100

    Update sol1.py

--- hackerrank/algo_and_ds/challenges/reverse_double_linked_list/sol1.py
+++ hackerrank/algo_and_ds/challenges/reverse_double_linked_list/sol1.py
@@ -70,3 +70,22 @@ def reverse(llist):
         
 
 if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        llist_count = int(input())
+
+        llist = DoublyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        llist1 = reverse(llist.head)
+
+        print_doubly_linked_list(llist1, ' ', fptr)
+        fptr.write('\n')
+
+    fptr.close()
